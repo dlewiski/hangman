773bbd49ebfa37b22f81957c3958cc0d4e478e14
David Lewis and Russell Hofvendahl <russell@>
2018-02-15 10:05:06 -0800

    add has_letter test, fails

--- spec/hangman_spec.rb
+++ spec/hangman_spec.rb
@@ -5,11 +5,16 @@ require 'pry'
 
 describe('Hangman') do
   describe('#initialize') do
-    game = Hangman.new
     it('generate a word from a preset list') do
+      game = Hangman.new
       expect(game.word_array.include?(game.word)).to(eq(true))
     end
   end
-  # describe('#present?')
-  # end
+  describe('#has_letter?') do
+    it('returns true if word contains letter, else false') do
+      game = Hangman.new
+      game.word = "ruby"
+      expect(game.has_letter?("r")).to(eq(true))
+    end
+  end
 end
